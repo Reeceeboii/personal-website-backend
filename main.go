04723d63d0baa9672cb106d7cd8159b6eeababc6
Reece Mercer <reecemercer981@gmail.com>
2021-04-10 01:56:35 +0100

    In memory GitHub data with periodic updates

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"compress/gzip"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"runtime"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -27,43 +26,17 @@ var awsSesh *session.Session
 // S3 specific session
 var s3svc *s3.S3
 
-/*
-   Generate the root HTML for any requests that land there. // TODO this needs to be updated when I get a minute.
-   At the minute it's just an (almost) direct port from my current backend.
-*/
+// Redirect any landing page requests
 func root(writer http.ResponseWriter, r *http.Request) {
-	writer.Header().Add("Content-Type", "text/html")
-	writer.Header().Set("Content-Encoding", "gzip")
-	tmpl, err := template.ParseFiles("./views/root.gohtml")
-	if err != nil {
-		log.Fatalf("Error creating template: %+v", err)
-	}
-
-	// pass the remote URL to the templater to create visitable URLs on the root page
-	data := struct {
-		RemoteURL string
-	}{
-		RemoteURL: "https://reecemercer-dev-backend.herokuapp.com",
-	}
-
-	// setup gzip
-	gz := gzip.NewWriter(writer)
-	defer gz.Close()
-
-	// generate templated HTML and send
-	tmpl.Execute(gz, data)
+	http.Redirect(writer, r, "https://reecemercer.dev", 303)
 }
 
-/*
-  404 route
-*/
+// 404 route
 func fourOhFour(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./views/404.gohtml")
+	http.Error(w, "Not found", http.StatusNotFound)
 }
 
-/*
-  Do some setup before server spins up
-*/
+// Do some setup before server spins up
 func init() {
 	// load environment variables
 	if err := godotenv.Load(); err != nil {
@@ -92,9 +65,7 @@ func (writer logger) Write(bytes []byte) (int, error) {
 	return fmt.Print(time.Now().UTC().Format("Jan _2 15:04:05.000000"), " [LOG] ", string(bytes))
 }
 
-/*
-  Simply output a log of each incoming request
-*/
+// Simply output a log of each incoming request
 func middlewareLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		log.Println("-> INCOMING", request.Method, "REQUEST TO", request.RequestURI)
@@ -103,9 +74,7 @@ func middlewareLogger(next http.Handler) http.Handler {
 	})
 }
 
-/*
-  Enable CORS headers on all responses
-*/
+// Enable CORS headers on all responses
 func middlewareCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		// insert CORS header into ResponseWriter
@@ -115,17 +84,24 @@ func middlewareCORS(next http.Handler) http.Handler {
 	})
 }
 
+// middleware to handle cache control headers on server responses
+func middlewareCache(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Cache-Control", fmt.Sprintf("%f", time.Hour.Seconds()))
+	})
+}
+
 func main() {
 	const base = "/api"
 
 	// create new Mux router
 	router := mux.NewRouter().StrictSlash(true)
 
-	//apply logging and CORS middleware
+	//apply logging, CORS and caching middleware
 	router.Use(middlewareLogger)
 	router.Use(middlewareCORS)
 
-	// root of server - serve the landing page
+	// root of server
 	router.HandleFunc("/", root).Methods("GET")
 
 	// GitHub API routes
@@ -136,16 +112,26 @@ func main() {
 	router.HandleFunc(base+"/photos/list-collections", listCollections).Methods("GET")
 	router.HandleFunc(base+"/photos/get-contents", getCollectionContents).Methods("GET")
 
-	// static file route
-	s := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
-	router.PathPrefix("/static/").Handler(s)
-
 	// and for any non matching routes, send 404 response back
 	router.NotFoundHandler = http.HandlerFunc(fourOhFour)
 
 	// setup logger
 	log.SetFlags(0)
 	log.SetOutput(new(logger))
+
+	// log some details when the server first spins up
+	log.Printf("Go runtime version: %s\n", runtime.Version())
+	log.Printf("AWS SDK: %s %s \n", aws.SDKName, aws.SDKVersion)
+	log.Printf("PORT: %s\n", os.Getenv("PORT"))
+	log.Printf("GITHUB_REFRESH_SECONDS: %s\n", os.Getenv("GITHUB_REFRESH_SECONDS"))
+	log.Printf("GO_ENV: %s\n", os.Getenv("GO_ENV"))
+
+	/*
+	  Start periodic updates from GitHub. We can force one starter update through when the server
+	  first executes else we'd be left without data until the first time the ticker fires
+	*/
+	mutexData.updateData(true)
+	go mutexData.updateData(false)
 
 	// start server and listen on port
 	log.Println("Listening!")
